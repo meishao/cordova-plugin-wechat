455c9e8a3b484095d1ca2a406bc47f74805f377e
vteam <vteam@vteam-no-MacBook-Air.local>
2015-12-13 20:04:24 +0800

    update file

---
+++ src/ios/CDVWeChat.h
@@ -0,0 +1,34 @@
+/*
+    Cordova WeChat Plugin
+    https://github.com/vilic/cordova-plugin-wechat
+
+    by VILIC VANE
+    https://github.com/vilic
+
+    MIT License
+*/
+
+#import <Cordova/CDV.h>
+#import "WXApi.h"
+
+enum CDVWeChatShareType {
+    CDVWeChatShareTypeApp = 1,
+    CDVWeChatShareTypeEmotion,
+    CDVWeChatShareTypeFile,
+    CDVWeChatShareTypeImage,
+    CDVWeChatShareTypeMusic,
+    CDVWeChatShareTypeVideo,
+    CDVWeChatShareTypeWebpage
+};
+
+@interface CDVWeChat: CDVPlugin <WXApiDelegate>
+
+@property (nonatomic, strong) NSString *currentCallbackId;
+@property (nonatomic, strong) NSString *wechatAppId;
+
+- (void)share:(CDVInvokedUrlCommand *)command;
+- (void)isInstalled:(CDVInvokedUrlCommand *)command;
+- (void)sendAuthRequest:(CDVInvokedUrlCommand *)command;
+- (void)sendPaymentRequest:(CDVInvokedUrlCommand *)command;
+
+@end
